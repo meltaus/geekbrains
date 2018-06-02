03805cb39108e66cd5d3b7debb6191ff85f021a9
meltaus <meltaus@yandex.ru>
2018-06-02 11:46:11 +0700

    Предположительно конечныйвариант обработки ошибки неерного типа данных

--- JavaCoreLesson2/src/main/java/gorbachev/javacore/lesson2/exeption/MyArrayDataException.java
+++ JavaCoreLesson2/src/main/java/gorbachev/javacore/lesson2/exeption/MyArrayDataException.java
@@ -1,4 +1,17 @@
 package gorbachev.javacore.lesson2.exeption;
 
-public class MyArrayDataException {
+public class MyArrayDataException extends Exception {
+
+    private static int row;
+    private static int column;
+
+    public MyArrayDataException(int row, int column) {
+        super("В ячейке не верный тип данных");
+        this.row = row;
+        this.column = column;
+    }
+
+    public MyArrayDataException(String msg) {
+        super(String.format("%s В строке %d, в ячейке %d не верный тип данных", msg, this.row, this.column));
+    }
 }
