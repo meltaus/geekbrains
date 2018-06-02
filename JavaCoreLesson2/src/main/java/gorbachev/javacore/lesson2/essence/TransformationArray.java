3be2b47cd303393f6d87ef9b690c596084de11d1
meltaus <meltaus@yandex.ru>
2018-06-02 12:12:51 +0700

    Конечная реализация класса трансформации массива рамера m*m. Реализация переопределения метода toString

--- JavaCoreLesson2/src/main/java/gorbachev/javacore/lesson2/essence/TransformationArray.java
+++ JavaCoreLesson2/src/main/java/gorbachev/javacore/lesson2/essence/TransformationArray.java
@@ -1,4 +1,58 @@
 package gorbachev.javacore.lesson2.essence;
 
+import gorbachev.javacore.lesson2.exeption.MyArrayDataException;
+import gorbachev.javacore.lesson2.exeption.MySizeArrayException;
+
+import java.util.Arrays;
+
 public class TransformationArray {
+
+    private int array[][];
+
+    public TransformationArray() {
+        System.out.println("Я не могу работать с пустотой");
+    }
+
+    public TransformationArray(String array[][]) {
+        createArray(array);
+    }
+
+    private void createArray(String array[][]) {
+        int size = array.length;
+        try {
+            if (size != 4) {
+                throw new MySizeArrayException();
+            } else {
+                this.array = new int[4][4];
+                for (int i = 0; i < size; i++) {
+                    if (array[i].length == 4) {
+                        for (int j = 0; j < 4; j++) {
+                            try {
+                                this.array[i][j] = Integer.parseInt(array[i][j]);
+                            } catch (NumberFormatException ex) {
+                                throw new MyArrayDataException(i, j);
+                            }
+                        }
+                    }
+                }
+            }
+        }catch (MySizeArrayException ex) {
+            System.out.println(ex);
+        } catch (MyArrayDataException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public String toString() {
+        int size = this.array.length;
+        String result = "";
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < this.array[i].length; j++) {
+                result += this.array[i][j] + "\t";
+            }
+            result += "\n";
+        }
+        return result;
+    }
 }
