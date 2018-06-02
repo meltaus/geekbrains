5a037c40d732fc89695e8abcc74e3f952716d4d8
meltaus <meltaus@yandex.ru>
2018-06-02 11:10:10 +0700

    Создаем строковый двумерный массив для работы и отправлем его в класс TransformationArray для последующей обработки.

--- JavaCoreLesson2/src/main/java/gorbachev/javacore/lesson2/Lesson2.java
+++ JavaCoreLesson2/src/main/java/gorbachev/javacore/lesson2/Lesson2.java
@@ -1,4 +1,28 @@
 package gorbachev.javacore.lesson2;
 
+import gorbachev.javacore.lesson2.essence.TransformationArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lesson2 {
+
+    public static void main(String[] args) {
+
+        TransformationArray transformationArray = new TransformationArray(createArray(4));
+    }
+
+
+    //Создаем строковый массив размерности m*m и заполняем случайными данными, которые можно приести к int
+    private static String[][] createArray(int size) {
+        String array[][] = new String[size][size];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++){
+                array[i][j] = Integer.toString(0 + (int)(Math.random() * ((1000 - 0) + 1)));
+            }
+        }
+
+        return array;
+    }
 }
