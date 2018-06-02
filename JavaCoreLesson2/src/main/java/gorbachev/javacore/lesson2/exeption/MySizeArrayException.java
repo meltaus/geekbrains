4c870da9209a7bba515c41a685f569dfda817e6e
meltaus <meltaus@yandex.ru>
2018-06-02 12:07:38 +0700

    Конечная реализация обработки ошибки размера массива

--- JavaCoreLesson2/src/main/java/gorbachev/javacore/lesson2/exeption/MySizeArrayException.java
+++ JavaCoreLesson2/src/main/java/gorbachev/javacore/lesson2/exeption/MySizeArrayException.java
@@ -2,11 +2,22 @@ package gorbachev.javacore.lesson2.exeption;
 
 public class MySizeArrayException extends Exception {
 
+    private static String msg;
+
     public MySizeArrayException(String msg) {
         super(msg);
+        this.msg = msg;
     }
 
     public MySizeArrayException() {
-        super("Вы пытаетесь создать массив не верного размера");
+    }
+
+    @Override
+    public String toString() {
+        if (this.msg != null) {
+            return msg;
+        } else {
+            return "Массив неверного размера";
+        }
     }
 }
