49658961bc73ddedd139fb708a1e9510a96ef81c
meltaus <meltaus@yandex.ru>
2018-06-07 00:56:34 +0700

    Запускаемый класс с примерами

--- JavaCoreLesson3/src/main/java/ru/gorbachev/javacore/lesson3/Lesson3.java
+++ JavaCoreLesson3/src/main/java/ru/gorbachev/javacore/lesson3/Lesson3.java
@@ -2,6 +2,7 @@ package ru.gorbachev.javacore.lesson3;
 
 import ru.gorbachev.javacore.lesson3.controller.SearchRepeating;
 import ru.gorbachev.javacore.lesson3.essence.ListWord;
+import ru.gorbachev.javacore.lesson3.essence.Phonebook;
 
 import java.util.ArrayList;
 
@@ -11,6 +12,18 @@ public class Lesson3 {
         ListWord listWord = new ListWord();
         SearchRepeating searchRepeating = new SearchRepeating(listWord.getArrayWord());
         searchRepeating.searchDuplicate();
-        System.out.println(searchRepeating.toString());
+        System.out.println(searchRepeating.toString() + "\n");
+
+        Phonebook phonebook = new Phonebook();
+        phonebook.add("Горбачев", 777777);
+        phonebook.add("Горбачев", 888888);
+        phonebook.add("Губанов", 4654654);
+        phonebook.add("Первушин", 1312314);
+        phonebook.add("Первушин", 1231569);
+        phonebook.add("Первушин", 123156);
+
+        System.out.println(phonebook.get("Горбачев"));
+        System.out.println(phonebook.get("Первушин"));
+        System.out.println(phonebook.get("Артем"));
     }
 }
