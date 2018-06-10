779e6a2f1d904446dfa0fc74b0c085058b5c1143
meltaus <meltaus@yandex.ru>
2018-06-10 21:19:45 +0700

    Создание формы + отрисовка формы + листенер на кнопку, который забирает текст с textEditorChat.

--- JavaCoreLesson4/src/main/java/ru/gorbachev/javacore/lesson4/Lesson4.java
+++ JavaCoreLesson4/src/main/java/ru/gorbachev/javacore/lesson4/Lesson4.java
@@ -1,4 +1,16 @@
 package ru.gorbachev.javacore.lesson4;
 
+import ru.gorbachev.javacore.lesson4.view.MainWindow;
+
+import javax.swing.*;
+
 public class Lesson4 {
+
+    public static void main (String[] args) {
+        JFrame mainWindows = new JFrame("Чат");
+        mainWindows.setContentPane(new MainWindow().getWindowPanel());
+        mainWindows.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainWindows.pack();
+        mainWindows.setVisible(true);
+    }
 }
--- JavaCoreLesson4/src/main/java/ru/gorbachev/javacore/lesson4/view/MainWindow.java
+++ JavaCoreLesson4/src/main/java/ru/gorbachev/javacore/lesson4/view/MainWindow.java
@@ -1,4 +1,55 @@
 package ru.gorbachev.javacore.lesson4.view;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class MainWindow {
+    private JPanel windowPanel;
+    private JButton sendBtn;
+    private JEditorPane textEditorChat;
+    private JTextPane textChatPane;
+
+
+    public MainWindow() {
+        textChatPane.setEditable(false);
+        sendBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Сообщение: " + textEditorChat.getText());
+                textEditorChat.setText("");
+            }
+        });
+    }
+
+    public JPanel getWindowPanel() {
+        return windowPanel;
+    }
+
+    public void setWindowPanel(JPanel windowPanel) {
+        this.windowPanel = windowPanel;
+    }
+
+    public JButton getSenBtn() {
+        return sendBtn;
+    }
+
+    public void setSenBtn(JButton senBtn) {
+        this.sendBtn = senBtn;
+    }
+
+    public JEditorPane getTextEditorChat() {
+        return textEditorChat;
+    }
+
+    public void setTextEditorChat(JEditorPane textEditorChat) {
+        this.textEditorChat = textEditorChat;
+    }
+
+    public JTextPane getTextChatPane() {
+        return textChatPane;
+    }
+
+    public void setTextChatPane(JTextPane textChatPane) {
+        this.textChatPane = textChatPane;
+    }
 }
