e0a67977cc214ca3b5cf86d22059d8b4b7922730
meltaus <meltaus@yandex.ru>
2018-06-12 15:49:18 +0700

    Объект, содержащий одномерный массив, каждый элемент которого заполнен "1"

--- JavaCoreLesson5/src/main/java/ru/gorbachev/javacore/lesson5/essence/OneArray.java
+++ JavaCoreLesson5/src/main/java/ru/gorbachev/javacore/lesson5/essence/OneArray.java
@@ -1,4 +1,26 @@
 package ru.gorbachev.javacore.lesson5.essence;
 
 public class OneArray {
+
+    static final int size = 10000000;
+    static final int h = size / 2;
+    float[] arr = new float[size];
+
+    public OneArray() {
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
+        }
+    }
+
+    public static int getSize() {
+        return size;
+    }
+
+    public static int getH() {
+        return h;
+    }
+
+    public float[] getArr() {
+        return arr;
+    }
 }
