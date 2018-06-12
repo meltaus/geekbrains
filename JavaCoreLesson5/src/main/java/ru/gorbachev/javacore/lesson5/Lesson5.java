0f3b0fc97e31fcebf69f9fa622b99f22755ce96f
meltaus <meltaus@yandex.ru>
2018-06-12 17:43:11 +0700

    Запускаемый класс с вызовом методов для решения задачи

--- JavaCoreLesson5/src/main/java/ru/gorbachev/javacore/lesson5/Lesson5.java
+++ JavaCoreLesson5/src/main/java/ru/gorbachev/javacore/lesson5/Lesson5.java
@@ -1,8 +1,14 @@
 package ru.gorbachev.javacore.lesson5;
 
+import ru.gorbachev.javacore.lesson5.mathArray.OneThread;
+import ru.gorbachev.javacore.lesson5.mathArray.TwoThread;
+
 public class Lesson5 {
 
     public static void main(String args[]) {
+        Thread oneThread = new Thread(new OneThread());
+        oneThread.start();
 
+        TwoThread twoThread = new TwoThread();
     }
 }
